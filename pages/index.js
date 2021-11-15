3b1badc31bceda19f420eabd0b77bc4e9b91edbd
KYEONGMIN CHO <ggingmin@gmail.com>
2021-11-15 19:25:30 +0900

    first commit

---
+++ pages/index.js
@@ -0,0 +1,28 @@
+import Head from 'next/head'
+import Layout, { siteTitle } from '../components/layout'
+import utilStyles from '../styles/utils.module.css'
+
+export default function Home() {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>LIVE AND LET LIVE!</p>
+        <p>
+          {' '}
+          <a href="https://ggingmin.notion.site/c6a97234524a46f786414b97caea6f98?v=f3e2c0b6c3e34c6794ad1abfbd5da31b">&#9889; Notion</a>
+        </p>
+        <p>
+          {' '}
+          <a href="https://github.com/ggingmin">&#128640; Github</a>
+        </p>
+        <p>  
+          {' '}
+          <a href="https://www.linkedin.com/in/ggingmin/">&#128293; Linkedin</a>
+        </p>
+      </section>
+    </Layout>
+  )
+}
